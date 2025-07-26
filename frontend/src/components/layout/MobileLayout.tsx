906a2959c6f0c9fb643e0ed0f5293e2c3a793e5a
Jeongmin Lee <clcl6084@gmail.com>
2025-07-27 00:41:45 +0900

    chore: add onLogoClick prop

--- frontend/src/components/layout/MobileLayout.tsx
+++ frontend/src/components/layout/MobileLayout.tsx
@@ -1,22 +1,25 @@
 import { ReactNode } from 'react'
-import { BottomNav } from './BottomNav'
-import { Header, HeaderProps } from './Header'
+import { HeaderProps } from './Header'
 
 interface MobileLayoutProps extends HeaderProps {
   children: ReactNode
+  onLogoClick?: () => void
 }
 
-export function MobileLayout({ children, ...props }: MobileLayoutProps) {
+export function MobileLayout({ children, onLogoClick, ...props }: MobileLayoutProps) {
   return (
     <div className="flex flex-col h-screen w-full justify-between">
       <div className="w-full h-16 flex items-center justify-center">
-          <h1 className="text-4xl font-bold tracking-wider text-white">BUYO</h1>
+          <h1 
+            className="text-4xl font-bold tracking-wider text-white cursor-pointer hover:opacity-80 transition-opacity"
+            onClick={onLogoClick}
+          >
+            BUYO
+          </h1>
       </div>
-      {/* <Header {...props} /> */}
       <main className="flex-1 overflow-y-auto px-4 py-2">
         {children}
       </main>
-      {/* <BottomNav /> */}
     </div>
   )
 }
