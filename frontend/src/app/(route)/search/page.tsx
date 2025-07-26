036bdce9bd2cdc4c0712fc1ddb4faf7bb9705c06
Jeongmin Lee <clcl6084@gmail.com>
2025-07-26 23:50:13 +0900

    feat: add stock feature

--- frontend/src/app/(route)/search/page.tsx
+++ frontend/src/app/(route)/search/page.tsx
@@ -1,9 +1,13 @@
 import { MobileLayout } from '@/components/layout/MobileLayout';
+import { StockChart } from '@/components/StockChart';
 
-export default function Home() {
+export default function SearchPage() {
   return (
     <MobileLayout type="back-search">
-      Search
+      <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">주식 검색</h1>
+        <StockChart stockCode="005930" />
+      </div>
     </MobileLayout>
   );
 }
--- frontend/src/components/layout/MobileLayout.tsx
+++ frontend/src/components/layout/MobileLayout.tsx
@@ -8,12 +8,15 @@ interface MobileLayoutProps extends HeaderProps {
 
 export function MobileLayout({ children, ...props }: MobileLayoutProps) {
   return (
-    <div className="flex flex-col h-screen max-w-[375px] mx-auto bg-background">
-      <Header {...props} />
+    <div className="flex flex-col h-screen w-full justify-between">
+      <div className="w-full h-16 flex items-center justify-center">
+          <h1 className="text-4xl font-bold tracking-wider text-white">BUYO</h1>
+      </div>
+      {/* <Header {...props} /> */}
       <main className="flex-1 overflow-y-auto px-4 py-2">
         {children}
       </main>
-      <BottomNav />
+      {/* <BottomNav /> */}
     </div>
   )
 }
