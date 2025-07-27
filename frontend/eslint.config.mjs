9aab6f074a8ace0cc6f510cd9ae41c4cfac75fae
Jeongmin Lee <clcl6084@gmail.com>
2025-07-27 10:16:16 +0900

    chore: ignore some rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -15,6 +15,9 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-require-imports": "off"
     },
   }
 ];
